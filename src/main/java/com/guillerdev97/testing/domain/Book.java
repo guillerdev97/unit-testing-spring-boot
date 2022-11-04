e5751897595743acdcd0374b2fcecc90ea4fae32
Guillermo García <guillermieres97@gmail.com>
2022-11-04 10:33:15 +0100

    create book model

---
+++ src/main/java/com/guillerdev97/testing/domain/Book.java
@@ -0,0 +1,33 @@
+package com.guillerdev97.testing.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book_records")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "book_id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "book_name")
+    private String name;
+
+    @NotNull
+    @Column(name = "book_summary")
+    private String summary;
+
+    @Column(name = "book_rating")
+    private int rating;
+}
