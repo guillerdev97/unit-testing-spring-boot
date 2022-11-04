e4817fa7a9f0bd8acfdb222435219b90f79fd9df
Guillermo García <guillermieres97@gmail.com>
2022-11-04 10:09:06 +0100

    feat: basic sum and multiply test done

---
+++ src/main/java/com/guillerdev97/testing/Calculator.java
@@ -0,0 +1,11 @@
+package com.guillerdev97.testing;
+
+public class Calculator {
+    public int multiplyTwoNumbers(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int sumTwoNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+}
