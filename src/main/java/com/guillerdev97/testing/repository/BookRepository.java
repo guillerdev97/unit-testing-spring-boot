8aaf5a693e1fc60e95dda8938f7b80e1ae2c3cce
Guillermo García <guillermieres97@gmail.com>
2022-11-19 11:03:52 +0100

    Delete BookRepository.java

--- src/main/java/com/guillerdev97/testing/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guillerdev97.testing.repository;
-
-import com.guillerdev97.testing.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
